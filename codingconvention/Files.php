f43b17838a3f960b61bf94bae5725801792df83d
huynhngoccanh <huynngoccanh9@gmail.com>
2017-08-16 21:40:44 +0700

    exercise coding convention

---
+++ codingconvention/Files.php
@@ -0,0 +1,11 @@
+<?php 
+
+	echo "xin chào";
+	echo '<br>';
+	function printName(){// tạo hàm in tên theo chuẩn camelCase
+		echo "tôi là cảnh";
+	}
+
+	printName();
+// cách viết này sai vì chứa cả declarations và side effects
+?>
---
+++ codingconvention/me/XinChao.php
@@ -0,0 +1,30 @@
+﻿<?php 
+namespace Me\Controller;
+
+class Hello
+{
+    public $name = "huynh ngoc canh";
+    public $number = 3;
+    public $listname = [];
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function listName()
+    {
+        $listname =[
+        "Huỳnh Ngọc Cảnh",
+        "Lê Văn Nam",
+        "Nguyễn Văn A", 
+        "Lê văn Tài"
+        ];
+        return $listname;
+    }
+    public function listDay()
+    {
+    	$listDay = [2, 3, 4, 5, 6, 7, 8];
+    	return $listDay;
+    }
+}
+
+?>
